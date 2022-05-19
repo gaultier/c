5283a88f8f27f953417990c97ed6fd88c7fffe52
Philippe Gaultier <philippe.gaultier@ppro.com>
2022-05-19 08:18:20 +0200

    [wip]

--- error.h
+++ error.h
@@ -8,12 +8,24 @@
 
 typedef struct {
   gbArray(gbString) call_stack;
+  gbArray(gbString) errors;
 } error;
 
-error* error_make() {
-  error* err = malloc(sizeof(error));
+static void error_record(error* err, gbString location, gbString message) {
   GB_ASSERT(err != NULL);
 
+  gb_array_append(err->call_stack, location);
+  gb_array_append(err->errors, message);
+}
+
+static error* error_make(gbAllocator allocator, gbString location,
+                         gbString message) {
+  error* err = gb_alloc(allocator, sizeof(error));
+  GB_ASSERT(err != NULL);
+
+  error_record(err, location, message);
   return err;
 }
-void error_record() {}
+
+#define error_make_location(allocator) \
+  gb_string_append_fmt("%s:%s:%s", __FILE__, __line__, __FUNCTION__)
