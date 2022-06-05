cd29db074faba8ab41a3ca136f7463ba99c1f84a
Philippe Gaultier <philippe.gaultier@ppro.com>
2022-06-05 07:53:35 +0200

    add dw_lang

--- c-crash-reporter/macho.c
+++ c-crash-reporter/macho.c
@@ -1,4 +1,3 @@
-#include <_types/_uint8_t.h>
 #define GB_IMPLEMENTATION
 #include <assert.h>
 #include <mach-o/loader.h>
@@ -949,6 +948,49 @@ typedef enum : uint8_t {
     DW_LNE_set_discriminator,
 } DW_LNE;
 
+typedef enum : uint16_t {
+    DW_LANG_C89 = 0x0001,
+    DW_LANG_C = 0x0002,
+    DW_LANG_Ada83 = 0x0003,
+    DW_LANG_C_plus_plus = 0x0004,
+    DW_LANG_Cobol74 = 0x0005,
+    DW_LANG_Cobol85 = 0x0006,
+    DW_LANG_Fortran77 = 0x0007,
+    DW_LANG_Fortran90 = 0x0008,
+    DW_LANG_Pascal83 = 0x0009,
+    DW_LANG_Modula2 = 0x000a,
+    DW_LANG_Java = 0x000b,
+    DW_LANG_C99 = 0x000c,
+    DW_LANG_Ada95 = 0x000d,
+    DW_LANG_Fortran95 = 0x000e,
+    DW_LANG_PLI = 0x000f,
+    DW_LANG_ObjC = 0x0010,
+    DW_LANG_ObjC_plus_plus = 0x0011,
+    DW_LANG_UPC = 0x0012,
+    DW_LANG_D = 0x0013,
+    DW_LANG_Python = 0x0014,
+    DW_LANG_OpenCL = 0x0015,
+    DW_LANG_Go = 0x0016,
+    DW_LANG_Modula3 = 0x0017,
+    DW_LANG_Haskell = 0x0018,
+    DW_LANG_C_plus_plus_03 = 0x0019,
+    DW_LANG_C_plus_plus_11 = 0x001a,
+    DW_LANG_OCaml = 0x001b,
+    DW_LANG_Rust = 0x001c,
+    DW_LANG_C11 = 0x001d,
+    DW_LANG_Swift = 0x001e,
+    DW_LANG_Julia = 0x001f,
+    DW_LANG_Dylan = 0x0020,
+    DW_LANG_C_plus_plus_14 = 0x0021,
+    DW_LANG_Fortran03 = 0x0022,
+    DW_LANG_Fortran08 = 0x0023,
+    DW_LANG_RenderScript = 0x0024,
+    DW_LANG_BLISS = 0x0025,
+    DW_LANG_Mips_Assembler = 0x8001,
+    DW_LANG_GOOGLE_RenderScript = 0x8e57,
+    DW_LANG_BORLAND_Delphi = 0xb000,
+} dw_lang;
+
 typedef struct {
 } dwarf_info;
 
@@ -1095,11 +1137,17 @@ static void read_dwarf_section_debug_info(void* data,
                     /* assert(str_index< */
                     break;
                 }
+                case DW_FORM_data2: {
+                    u16 lang = *(u16*)&data[offset];
+                    offset += 2;
+                    printf("DW_FORM_data2: %#x\n", lang);
+                    assert(lang == DW_LANG_C99);
+                    break;
+                }
                 default:
                     assert(0 && "UNIMPLEMENTED");
             }
         }
-        break;
     }
 }
 
