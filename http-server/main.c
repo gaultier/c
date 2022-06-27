0bc9dfee1f65bc2d876c8e901a42669441bf1f48
Philippe Gaultier <philippe.gaultier@ppro.com>
2022-06-27 08:42:00 +0200

    Fork

--- http-server/main.c
+++ http-server/main.c
@@ -1,17 +1,64 @@
 #include <errno.h>
 #include <netinet/in.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
+#define GB_IMPLEMENTATION
+#define GB_STATIC
+#include "../vendor/gb.h"
+
+#define IP_ADDR_STR_LEN 17
+
 static void ip(uint32_t val, char* res) {
     uint8_t a = val >> 24, b = val >> 16, c = val >> 8, d = val & 0xff;
     snprintf(res, 16, "%hhu.%hhu.%hhu.%hhu", d, c, b, a);
 }
 
-int main() {
+static void print_usage(int argc, char* argv[]) {
+    GB_ASSERT(argc > 0);
+    printf("%s <port>\n", argv[0]);
+}
+
+static int handle_connection(struct sockaddr_in client_addr, int conn_fd) {
+    char ip_addr[IP_ADDR_STR_LEN] = "";
+    ip(client_addr.sin_addr.s_addr, ip_addr);
+    printf("New connection: %s:%hu\n", ip_addr, client_addr.sin_port);
+
+    int sent = send(conn_fd, "hello!", 6, 0);
+    if (sent == -1) {
+        fprintf(stderr, "Failed to send(2): addr=%s:%hu err=%s\n", ip_addr,
+                client_addr.sin_port, strerror(errno));
+        return errno;
+    }
+    sleep(5);
+
+    int err = 0;
+    if ((err = close(conn_fd)) != 0) {
+        fprintf(stderr, "Failed to close socket for: err=%s\n",
+                strerror(errno));
+        return err;
+    }
+
+    puts("finished");
+    return 0;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        print_usage(argc, argv);
+        return 0;
+    }
+
+    const u64 port = gb_str_to_u64(argv[1], NULL, 10);
+    if (port > UINT16_MAX) {
+        fprintf(stderr, "Invalid port number: %llu\n", port);
+        return 1;
+    }
+
     int sock_fd = socket(PF_INET, SOCK_STREAM, 0);
     if (sock_fd == -1) {
         fprintf(stderr, "Failed to socket(2): %s\n", strerror(errno));
@@ -50,18 +97,16 @@ int main() {
             fprintf(stderr, "Failed to accept(2): %s\n", strerror(errno));
             return errno;
         }
-        char ip_addr[17] = "";
-        ip(client_addr.sin_addr.s_addr, ip_addr);
-        printf("New connection: %s:%hu\n", ip_addr, client_addr.sin_port);
 
-        int sent = send(conn_fd, "hello!", 6, 0);
-        if (sent == -1) {
-            fprintf(stderr, "Failed to send(2): addr=%s:%hu err=%s\n", ip_addr,
-                    client_addr.sin_port, strerror(errno));
-            return errno;
+        pid_t pid = fork();
+        if (pid == -1) {
+            fprintf(stderr, "Failed to fork(2): err=%s\n", strerror(errno));
+        } else if (pid == 0) {  // Child
+            exit(handle_connection(client_addr, conn_fd));
+        } else {  // Parent
+            // Fds are duplicated by fork(2) and need to be
+            // closed by both parent & child
+            close(conn_fd);
         }
-        sleep(10);
-
-        close(conn_fd);
     }
 }
