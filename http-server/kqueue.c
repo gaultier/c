739e0bda95430805e82404b32f2084fbb3c74311
Philippe Gaultier <philippe.gaultier@ppro.com>
2022-06-29 13:01:04 +0200

    extract

--- http-server/kqueue.c
+++ http-server/kqueue.c
@@ -30,15 +30,22 @@ typedef struct {
     gbArray(struct kevent) event_list;
 } server;
 
-static void conn_handle_init(conn_handle* ch, gbAllocator allocator) {
-    ch->req = gb_string_make_reserve(allocator, 4096);
-    ch->res = gb_string_make_reserve(allocator, 4096);
+static void ip(uint32_t val, char* res) {
+    uint8_t a = val >> 24, b = val >> 16, c = val >> 8, d = val & 0xff;
+    snprintf(res, 16, "%hhu.%hhu.%hhu.%hhu", d, c, b, a);
 }
 
-static int conn_handle_read_req(conn_handle* ch) {
+static void conn_handle_init(conn_handle* ch, gbAllocator allocator,
+                             struct sockaddr_in client_addr) {
+    ch->req = gb_string_make_reserve(allocator, 4096);
+    ch->res = gb_string_make_reserve(allocator, 4096);
+    ip(client_addr.sin_addr.s_addr, ch->ip);
+}
+
+static int conn_handle_read_request(conn_handle* ch) {
     const ssize_t received = read(ch->fd, ch->buf, CONN_BUF_LEN);
     if (received == -1) {
-        fprintf(stderr, "Failed to recv(2): ip=%shu err=%s\n", ch->ip,
+        fprintf(stderr, "Failed to read(2): ip=%shu err=%s\n", ch->ip,
                 strerror(errno));
         return errno;
     }
@@ -56,7 +63,9 @@ static void server_add_socket_to_watch_list(server* s, int fd) {
            EV_ADD | EV_CLEAR, 0, 0, 0);
 }
 
-static void server_init(server* s) {
+static void server_init(server* s, gbAllocator allocator) {
+    s->allocator = allocator;
+
     gb_array_init_reserve(s->watch_list, s->allocator, LISTEN_BACKLOG);
     printf("[D001] sock_fd=%d\n", s->fd);
 
@@ -65,13 +74,6 @@ static void server_init(server* s) {
                           gb_array_capacity(s->watch_list));
 
     gb_array_init_reserve(s->conn_handles, s->allocator, LISTEN_BACKLOG);
-
-    server_add_socket_to_watch_list(s, s->fd);
-}
-
-static void ip(uint32_t val, char* res) {
-    uint8_t a = val >> 24, b = val >> 16, c = val >> 8, d = val & 0xff;
-    snprintf(res, 16, "%hhu.%hhu.%hhu.%hhu", d, c, b, a);
 }
 
 static void print_usage(int argc, char* argv[]) {
@@ -97,7 +99,7 @@ static int server_accept_new_connection(server* s) {
     server_add_socket_to_watch_list(s, conn_fd);
 
     conn_handle ch = {.fd = conn_fd};
-    conn_handle_init(&ch, s->allocator);
+    conn_handle_init(&ch, s->allocator, client_addr);
     gb_array_append(s->conn_handles, ch);
     return -1;
 }
@@ -110,7 +112,94 @@ static conn_handle* server_find_conn_handle_by_fd(server* s, int fd) {
     return NULL;
 }
 
-static int server_run(server* s) {
+static void conn_handle_make_response(conn_handle* ch) {
+    const char msg[] = "hello";
+    bzero(ch->buf, CONN_BUF_LEN);
+    snprintf(ch->buf, CONN_BUF_LEN,
+             "HTTP/1.1 200 OK\r\n"
+             "Content-Type: text/plain; charset=utf8\r\n"
+             "Content-Length: %td\r\n"
+             "\r\n"
+             "%s",
+             sizeof(msg), msg);
+}
+
+static int conn_handle_send_response(conn_handle* ch) {
+    const int nb = write(ch->fd, ch->buf, strlen(ch->buf));
+    if (nb == -1) {
+        fprintf(stderr, "Failed to write(2): ip=%s err=%s\n", ch->ip,
+                strerror(errno));
+        return errno;
+    }
+    // TODO: check if all bytes were written
+
+    return nb;
+}
+
+static void server_remove_connection(server* s, conn_handle* ch) {
+    close(ch->fd);
+
+    // Remove watch
+    for (int i = 0; i < gb_array_count(s->watch_list); i++) {
+        if (s->watch_list[i].ident == ch->fd) {
+            s->watch_list[i] = s->watch_list[gb_array_count(s->watch_list) - 1];
+            gb_array_pop(s->watch_list);
+            break;
+        }
+    }
+
+    // Remove handle
+    for (int i = 0; i < gb_array_count(s->conn_handles); i++) {
+        if (&s->conn_handles[i] == ch) {
+            s->conn_handles[i] =
+                s->conn_handles[gb_array_count(s->conn_handles) - 1];
+            gb_array_pop(s->conn_handles);
+            break;
+        }
+    }
+}
+
+static int server_listen_and_bind(server* s, u16 port) {
+    int err = 0;
+
+    s->fd = socket(PF_INET, SOCK_STREAM, 0);
+    if (s->fd == -1) {
+        fprintf(stderr, "Failed to socket(2): %s\n", strerror(errno));
+        return errno;
+    }
+
+    int val = 1;
+    if ((err = setsockopt(s->fd, SOL_SOCKET, SO_REUSEADDR, &val,
+                          sizeof(val))) == -1) {
+        fprintf(stderr, "Failed to setsockopt(2): %s\n", strerror(errno));
+        return errno;
+    }
+
+    const struct sockaddr_in addr = {
+        .sin_family = AF_INET,
+        .sin_port = htons(port),
+    };
+
+    if ((err = bind(s->fd, (const struct sockaddr*)&addr, sizeof(addr))) ==
+        -1) {
+        fprintf(stderr, "Failed to bind(2): %s\n", strerror(errno));
+        return errno;
+    }
+
+    if ((err = listen(s->fd, 16 * 1024)) == -1) {
+        fprintf(stderr, "Failed to listen(2): %s\n", strerror(errno));
+        return errno;
+    }
+    server_add_socket_to_watch_list(s, s->fd);
+    printf("Listening: :%d\n", port);
+    return 0;
+}
+
+static int server_run(server* s, u16 port) {
+    int err = 0;
+    err = server_listen_and_bind(s, port);
+    if (err != 0) return err;
+
     const int queue = kqueue();
     if (queue == -1) {
         fprintf(stderr, "%s:%d:Failed to create queue with kqueue(): %s\n",
@@ -145,107 +234,15 @@ static int server_run(server* s) {
             printf("[D008] Data to be read on: %d\n", fd);
             conn_handle* ch = server_find_conn_handle_by_fd(s, fd);
             assert(ch != NULL);
-            conn_handle_read_req(ch);
+            conn_handle_read_request(ch);
+            conn_handle_make_response(ch);
+            conn_handle_send_response(ch);
+            server_remove_connection(s, ch);
+            // TODO: cleanup, errors
         }
-
-        // Remove watch
-        // for (int i = 0; i < gb_array_count(watch_list); i++) {
-        //    if (watch_list[i].ident == conn_fd) {
-        //        watch_list[i] = watch_list[gb_array_count(watch_list) - 1];
-        //        gb_array_pop(watch_list);
-        //        break;
-        //    }
-        //}
     }
     return -1;
 }
-
-// static int handle_connection(struct sockaddr_in client_addr, int conn_fd) {
-//     char ip_addr[IP_ADDR_STR_LEN] = "";
-//     ip(client_addr.sin_addr.s_addr, ip_addr);
-//     printf("New connection: %s:%hu\n", ip_addr, client_addr.sin_port);
-//
-//     char conn_buf[CONN_BUF_LEN] = "";
-//
-//     http_parser_settings settings = {.on_url = on_url};
-//     http_parser parser = {0};
-//     http_parser_init(&parser, HTTP_REQUEST);
-//
-//     static u8 mem[10 * 1024 /* 10KiB */] = {};
-//     gbArena arena = {0};
-//     gb_arena_init_from_memory(&arena, mem, sizeof(mem));
-//     gbAllocator allocator = gb_arena_allocator(&arena);
-//     gbString url = gb_string_make_reserve(allocator, 100);
-//     gbString req = gb_string_make_reserve(allocator, 4096);
-//     parser.data = url;
-//     int err = 0;
-//
-//     while (1) {
-//         ssize_t received = recv(conn_fd, conn_buf, CONN_BUF_LEN, 0);
-//         if (received == -1) {
-//             fprintf(stderr, "Failed to recv(2): addr=%s:%hu err=%s\n",
-//             ip_addr,
-//                     client_addr.sin_port, strerror(errno));
-//             err = errno;
-//             goto end;
-//         }
-//         if (received == 0) {  // Client closed connection
-//             goto end;
-//         }
-//         req = gb_string_append_length(req, conn_buf, received);
-//
-//         const isize len = gb_string_length(req);
-//         // End of request ?
-//         // TODO: limit on received bytes total
-//         if (len >= 4 && req[len - 4] == '\r' && req[len - 3] == '\n' &&
-//             req[len - 2] == '\r' && req[len - 1] == '\n') {
-//             break;
-//         }
-//     }
-//
-//     int nparsed =
-//         http_parser_execute(&parser, &settings, req, gb_string_length(req));
-//
-//     if (parser.upgrade) {
-//         GB_ASSERT_MSG(0, "Unimplemented");
-//     } else if (nparsed != gb_string_length(req)) {
-//         fprintf(stderr,
-//                 "Failed to parse http request: addr=%s:%hu nparsed=%d "
-//                 "received=%zd\n",
-//                 ip_addr, client_addr.sin_port, nparsed,
-//                 gb_string_length(req));
-//         goto end;
-//     }
-//
-//     // Response
-//     bzero(conn_buf, CONN_BUF_LEN);
-//     snprintf(conn_buf, CONN_BUF_LEN,
-//              "HTTP/1.1 200 OK\r\n"
-//              "Content-Type: text/plain; charset=utf8\r\n"
-//              "Content-Length: %td\r\n"
-//              "\r\n"
-//              "%s",
-//              GB_STRING_HEADER(url)->length, url);
-//
-//     // TODO: send in loop
-//     int sent = send(conn_fd, conn_buf, strlen(conn_buf), 0);
-//     if (sent == -1) {
-//         fprintf(stderr, "Failed to send(2): addr=%s:%hu err=%s\n", ip_addr,
-//                 client_addr.sin_port, strerror(errno));
-//         err = errno;
-//         goto end;
-//     }
-//
-// end:
-//     gb_string_free(url);
-//     if ((err = close(conn_fd)) != 0) {
-//         fprintf(stderr, "Failed to close socket for: err=%s\n",
-//                 strerror(errno));
-//         err = errno;
-//     }
-//
-//     return 0;
-// }
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
@@ -260,38 +257,9 @@ int main(int argc, char* argv[]) {
     }
 
     int err = 0;
-
-    int sock_fd = socket(PF_INET, SOCK_STREAM, 0);
-    if (sock_fd == -1) {
-        fprintf(stderr, "Failed to socket(2): %s\n", strerror(errno));
-        return errno;
-    }
-
-    int val = 1;
-    if ((err = setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR, &val,
-                          sizeof(val))) == -1) {
-        fprintf(stderr, "Failed to setsockopt(2): %s\n", strerror(errno));
-        return errno;
-    }
-
-    const struct sockaddr_in addr = {
-        .sin_family = AF_INET,
-        .sin_port = htons(12345),
-    };
-
-    if ((err = bind(sock_fd, (const struct sockaddr*)&addr, sizeof(addr))) ==
-        -1) {
-        fprintf(stderr, "Failed to bind(2): %s\n", strerror(errno));
-        return errno;
-    }
-
-    if ((err = listen(sock_fd, 16 * 1024)) == -1) {
-        fprintf(stderr, "Failed to listen(2): %s\n", strerror(errno));
-        return errno;
-    }
-
     gbAllocator allocator = gb_heap_allocator();
-    server s = {.fd = sock_fd, .allocator = allocator};
-    server_init(&s);
-    server_run(&s);
+    server s = {0};
+    server_init(&s, allocator);
+    err = server_run(&s, port);
+    if (err != 0) return err;
 }
