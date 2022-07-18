b991697ce4be4ae3dc1bf14e6882144d46142777
Philippe Gaultier <philippe.gaultier@ppro.com>
2022-07-18 15:34:11 +0200

    fix

--- opentelemetry-jaeger/main.c
+++ opentelemetry-jaeger/main.c
@@ -5,19 +5,18 @@ int main() {
 
     const __uint128_t trace_id = ot_generate_trace_id();
 
-    ot_span_t* span_a =
-        ot_span_create_c(trace_id, "span-a", OT_SK_CLIENT, OT_ST_OUT_OF_RANGE,
-                         "this is the first span", 0);
+    ot_span_t* span_a = ot_span_create(trace_id, "span-a", OT_SK_CLIENT,
+                                       "this is the first span", 0);
 
     ot_span_t* span_b =
-        ot_span_create_c(trace_id, "span-b", OT_SK_CLIENT, OT_ST_OUT_OF_RANGE,
-                         "this is the second span", span_a->trace_id);
+        ot_span_create(trace_id, "span-b", OT_SK_CLIENT,
+                       "this is the second span", span_a->trace_id);
 
     usleep(8);
     ot_span_end(span_b);
-    ot_span_t* span_c = ot_span_create_c(
-        trace_id, "span-c", OT_SK_SERVER, OT_ST_FAILED_PRECONDITION,
-        "this is the third span", span_a->trace_id);
+    ot_span_t* span_c =
+        ot_span_create(trace_id, "span-c", OT_SK_SERVER,
+                       "this is the third span", span_a->trace_id);
     usleep(10);
     ot_span_end(span_c);
     usleep(5);
