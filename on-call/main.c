dbe99f410fcf0f59b5842ea1040aee2437d712c5
Philippe Gaultier <philippe.gaultier@ppro.com>
2022-07-19 14:04:00 +0200

    rename

--- on-call/main.c
+++ on-call/main.c
@@ -23,7 +23,7 @@ typedef struct {
     int month, year;  // In ISO range
     u16 week_money, week_hours, week_end_money, week_end_hours, total_hours,
         total_money;
-} bill_summary_t;
+} monthly_bill_t;
 
 static bool datetime_is_week_end(const struct tm* d) {
     return d->tm_wday == /* Sunday */ 0 || d->tm_wday == /* Saturday */ 6;
@@ -34,19 +34,19 @@ static bool datetime_is_working_hour(const struct tm* d) {
     return !datetime_is_week_end(d) && 9 <= d->tm_hour && d->tm_hour < 18;
 }
 
-static void shift_bill_hour(bill_summary_t* summary, time_t timestamp) {
+static void shift_bill_hour(monthly_bill_t* monthly_bill, time_t timestamp) {
     struct tm* hour = localtime(&timestamp);
     if (datetime_is_working_hour(hour)) return;
 
-    summary->total_hours += 1;
+    monthly_bill->total_hours += 1;
     if (datetime_is_week_end(hour)) {
-        summary->week_end_hours += 1;
-        summary->week_end_money += hourly_week_end_rate;
-        summary->total_money += hourly_week_end_rate;
+        monthly_bill->week_end_hours += 1;
+        monthly_bill->week_end_money += hourly_week_end_rate;
+        monthly_bill->total_money += hourly_week_end_rate;
     } else {
-        summary->week_hours += 1;
-        summary->week_money += hourly_week_rate;
-        summary->total_money += hourly_week_rate;
+        monthly_bill->week_hours += 1;
+        monthly_bill->week_money += hourly_week_rate;
+        monthly_bill->total_money += hourly_week_rate;
     }
 }
 
@@ -58,23 +58,24 @@ static time_t get_start_of_next_month(const struct tm* d) {
     return timelocal(&tmp);
 }
 
-static void shift_bill_monthly(gbArray(bill_summary_t) summaries,
+static void bill_shift_monthly(gbArray(monthly_bill_t) monthly_bills,
                                datetime_range_t* shift) {
-    bill_summary_t* summary = NULL;
+    monthly_bill_t* monthly_bill = NULL;
     // Upsert
     {
-        const u64 summaries_len = gb_array_count(summaries);
-        if (summaries_len > 0 &&
-            (summaries[summaries_len - 1].month - 1) == shift->start.tm_mon &&
-            (summaries[summaries_len - 1].year - 1900) ==
+        const u64 monthly_bills_len = gb_array_count(monthly_bills);
+        if (monthly_bills_len > 0 &&
+            (monthly_bills[monthly_bills_len - 1].month - 1) ==
+                shift->start.tm_mon &&
+            (monthly_bills[monthly_bills_len - 1].year - 1900) ==
                 shift->start.tm_year) {
-            summary = &summaries[summaries_len - 1];
+            monthly_bill = &monthly_bills[monthly_bills_len - 1];
         } else {
             gb_array_append(
-                summaries,
-                ((bill_summary_t){.month = shift->start.tm_mon + 1,
+                monthly_bills,
+                ((monthly_bill_t){.month = shift->start.tm_mon + 1,
                                   .year = shift->start.tm_year + 1900}));
-            summary = &summaries[gb_array_count(summaries) - 1];
+            monthly_bill = &monthly_bills[gb_array_count(monthly_bills) - 1];
         }
     }
 
@@ -85,7 +86,7 @@ static void shift_bill_monthly(gbArray(bill_summary_t) summaries,
 
     // Bill from the start of the shift to the end of the month
     while (timestamp < MIN(end_timestamp, start_of_next_month)) {
-        shift_bill_hour(summary, timestamp);
+        shift_bill_hour(monthly_bill, timestamp);
         timestamp += 3600;
     }
     if (shift->start.tm_mon == shift->end.tm_mon) return;
@@ -93,37 +94,37 @@ static void shift_bill_monthly(gbArray(bill_summary_t) summaries,
     // Handle the case of a shift spanning 2 months so it appears in 2 monthly
     // bills
     assert(shift->start.tm_mon + 1 == shift->end.tm_mon);
-    gb_array_append(summaries,
-                    ((bill_summary_t){.month = shift->end.tm_mon + 1,
+    gb_array_append(monthly_bills,
+                    ((monthly_bill_t){.month = shift->end.tm_mon + 1,
                                       .year = shift->end.tm_year + 1900}));
-    summary = &summaries[gb_array_count(summaries) - 1];
+    monthly_bill = &monthly_bills[gb_array_count(monthly_bills) - 1];
 
     // Bill from the first of the month to the end of the shift
     while (timestamp < end_timestamp) {
-        shift_bill_hour(summary, timestamp);
+        shift_bill_hour(monthly_bill, timestamp);
         timestamp += 3600;
     }
 }
 
-static void bill(datetime_range_t* work, u64 work_len) {
-    assert(work != NULL);
-    assert(work_len > 0);
+static void bill_shifts(datetime_range_t* shifts, u64 shifts_len) {
+    assert(shifts != NULL);
+    assert(shifts_len > 0);
 
-    gbArray(bill_summary_t) summaries = NULL;
-    gb_array_init_reserve(summaries, gb_heap_allocator(), 100);
+    gbArray(monthly_bill_t) monthly_bills = NULL;
+    gb_array_init_reserve(monthly_bills, gb_heap_allocator(), 100);
 
-    for (int i = 0; i < work_len; i++) {
-        shift_bill_monthly(summaries, &work[i]);
+    for (int i = 0; i < shifts_len; i++) {
+        bill_shift_monthly(monthly_bills, &shifts[i]);
     }
 
     // TODO: better formatting
-    for (int i = 0; i < gb_array_count(summaries); i++) {
-        __builtin_dump_struct(&summaries[i], &printf);
+    for (int i = 0; i < gb_array_count(monthly_bills); i++) {
+        __builtin_dump_struct(&monthly_bills[i], &printf);
     }
 }
 
 int main() {
-    datetime_range_t work[] = {
+    datetime_range_t shifts[] = {
         {DATE_TIME(2021, 9, 18, 9), DATE_TIME(2021, 9, 24, 9)},
         {DATE_TIME(2021, 10, 1, 18), DATE_TIME(2021, 10, 7, 9)},
         {DATE_TIME(2021, 11, 21, 9), DATE_TIME(2021, 11, 26, 9)},
@@ -140,5 +141,5 @@ int main() {
         {DATE_TIME(2022, 7, 20, 18), DATE_TIME(2022, 7, 22, 9)},
         {DATE_TIME(2022, 7, 22, 20), DATE_TIME(2022, 7, 23, 9)},
     };
-    bill(work, sizeof(work) / sizeof(work[0]));
+    bill_shifts(shifts, sizeof(shifts) / sizeof(shifts[0]));
 }
