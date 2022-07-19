d99fba661e3429ef9ec4161de7d7b995f59a39f3
Philippe Gaultier <philippe.gaultier@ppro.com>
2022-07-19 08:02:49 +0200

    split .c/.h

--- opentelemetry-jaeger/main.c
+++ opentelemetry-jaeger/main.c
@@ -1,3 +1,6 @@
+#include <stdlib.h>
+#include <unistd.h>
+
 #include "opentelemetry.h"
 
 int main() {
@@ -5,21 +8,21 @@ int main() {
 
     const __uint128_t trace_id = ot_generate_trace_id();
 
-    ot_span_t* span_a = ot.span_create_root(trace_id, "span-a", OT_SK_CLIENT,
+    ot_span_t* span_a = ot_span_create_root(trace_id, "span-a", OT_SK_CLIENT,
                                             "this is the first span");
 
-    ot_span_t* span_b = ot.span_create_child_of(
+    ot_span_t* span_b = ot_span_create_child_of(
         trace_id, "span-b", OT_SK_CLIENT, "this is the second span", span_a);
-    ot.span_add_attribute(span_b, "service.name", "clone-gitlab-api");
+    ot_span_add_attribute(span_b, "service.name", "clone-gitlab-api");
 
     usleep(8);
-    ot.span_end(span_b);
-    ot_span_t* span_c = ot.span_create_child_of(
+    ot_span_end(span_b);
+    ot_span_t* span_c = ot_span_create_child_of(
         trace_id, "span-c", OT_SK_SERVER, "this is the third span", span_a);
     usleep(10);
-    ot.span_end(span_c);
+    ot_span_end(span_c);
     usleep(5);
-    ot.span_end(span_a);
+    ot_span_end(span_a);
 
-    ot.end();
+    ot_end();
 }
