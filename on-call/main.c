3419aa284f40874874218abf3e72b08648673f27
Philippe Gaultier <philippe.gaultier@ppro.com>
2022-07-19 13:16:20 +0200

    oncall

--- on-call/main.c
+++ on-call/main.c
@@ -131,6 +131,7 @@ int main() {
         {DATE_TIME(2022, 7, 6, 18), DATE_TIME(2022, 7, 9, 0)},
         {DATE_TIME(2022, 7, 10, 0), DATE_TIME(2022, 7, 11, 9)},
         {DATE_TIME(2022, 7, 20, 18), DATE_TIME(2022, 7, 22, 9)},
+        {DATE_TIME(2022, 7, 22, 20), DATE_TIME(2022, 7, 23, 9)},
     };
     bill(work, sizeof(work) / sizeof(work[0]));
 }
