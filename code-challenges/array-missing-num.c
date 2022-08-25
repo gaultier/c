113974ccd7c116c7f484c795553e641bbab6e6f2
Philippe Gaultier <philippe.gaultier@ppro.com>
2022-08-25 08:09:16 +0200

    array-missing-num.c

---
+++ code-challenges/array-missing-num.c
@@ -0,0 +1,22 @@
+#include <_types/_uint64_t.h>
+#include <stdint.h>
+#include <stdio.h>
+
+uint64_t find_missing_num(uint64_t* nums, uint64_t nums_len) {
+    uint64_t i = 0, j = 0;
+    for (i = 1; i <= nums_len; i++) {
+        for (j = 0; j < nums_len; j++) {
+            if (i == nums[j]) goto found;
+        }
+        return i;
+
+    found : {}
+    }
+    __builtin_unreachable();
+}
+
+int main() {
+    uint64_t nums[] = {3, 7, 1, 2, 8, 4, 5};
+
+    printf("%llu\n", find_missing_num(nums, sizeof(nums) / sizeof(nums[0])));
+}
