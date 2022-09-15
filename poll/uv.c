db7646ec01390640e42d9b6605619b3e0939542d
Philippe Gaultier <philippe.gaultier@ppro.com>
2022-09-15 13:21:10 +0200

    arrays

--- poll/uv.c
+++ poll/uv.c
@@ -1,7 +1,6 @@
-#include "uv.h"
-
 #include <assert.h>
 #include <netinet/in.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <uv.h>
@@ -22,54 +21,27 @@ static void on_connect(uv_connect_t* handle, int status) {
     printf("[%s] on_connect: %d\n", (char*)handle->handle->data, status);
 }
 
-static char* ip1 = "87.148.1.40";
-static char* ip2 = "72.235.131.120";
-static char* ip3 = "0.0.0.0";
-static uv_tcp_t server1;
-static uv_tcp_t server2;
-static uv_tcp_t server3;
-static uv_connect_t req1;
-static uv_connect_t req2;
-static uv_connect_t req3;
-
 int main() {
     uv_loop_t* loop = uv_default_loop();
+    char* ips[] = {"87.148.1.40", "72.235.131.120", "127.0.0.1"};
+    uint16_t ports[] = {6881, 6881, 8081};
 
-    {
-        int r = uv_tcp_init(uv_default_loop(), &server1);
+    uv_tcp_t servers[3] = {0};
+    uv_connect_t reqs[3] = {0};
+    for (int i = 0; i < sizeof(ips) / sizeof(ips[0]); i++) {
+        int r = uv_tcp_init(uv_default_loop(), &servers[i]);
+        assert(r == 0);
         struct sockaddr_in addr = {0};
-        uv_ip4_addr("87.148.1.40", 6881, &addr);
+        uv_ip4_addr(ips[i], ports[i], &addr);
 
-        req1.data = server1.data = ip1;
-        r = uv_tcp_connect(&req1, &server1, (void*)&addr, on_connect);
+        reqs[i].data = servers[i].data = ips[i];
+        printf("[%s:%d] Connecting\n", ips[i], ports[i]);
+        r = uv_tcp_connect(&reqs[i], &servers[i], (void*)&addr, on_connect);
         assert(r == 0);
 
-        r = uv_read_start((uv_stream_t*)&server1, my_alloc_cb, on_read);
+        r = uv_read_start((uv_stream_t*)&servers[i], my_alloc_cb, on_read);
         assert(r == 0);
     }
-    {
-        int r = uv_tcp_init(uv_default_loop(), &server2);
-        struct sockaddr_in addr = {0};
-        uv_ip4_addr("72.235.131.120", 6881, &addr);
 
-        req2.data = server2.data = ip2;
-        r = uv_tcp_connect(&req2, &server2, (void*)&addr, on_connect);
-        assert(r == 0);
-
-        r = uv_read_start((uv_stream_t*)&server2, my_alloc_cb, on_read);
-        assert(r == 0);
-    }
-    {
-        int r = uv_tcp_init(uv_default_loop(), &server3);
-        struct sockaddr_in addr = {0};
-        uv_ip4_addr("0.0.0.0", 8081, &addr);
-
-        req3.data = server3.data = ip3;
-        r = uv_tcp_connect(&req3, &server3, (void*)&addr, on_connect);
-        assert(r == 0);
-
-        r = uv_read_start((uv_stream_t*)&server3, my_alloc_cb, on_read);
-        assert(r == 0);
-    }
     uv_run(loop, UV_RUN_DEFAULT);
 }
