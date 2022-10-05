52fbb82552f74590c9031363db9f19c5a7410e26
Philippe Gaultier <philippe.gaultier@ppro.com>
2022-10-05 14:00:33 +0200

    fix

--- nvim/client.vim
+++ nvim/client.vim
@@ -1,6 +1,10 @@
-let chan = sockconnect('tcp', '0.0.0.0:12345',{} )
-echo 'requesting'
-" let result = rpcrequest(chan, 'hello/world')
-call chansend(chan, "hello world")
-call chanclose(chan)
-echo 'requested'
+function! s:copy_gitlab_url()
+  echo "foO"
+  let chan = sockconnect('tcp', '0.0.0.0:12345',{} )
+  let file_path = expand('%:p')
+  let line=line('.')
+  call chansend(chan, [file_path, line])
+  call chanclose(chan)
+endfunction
+
+nnoremap <leader>x :call <SID>copy_gitlab_url()<cr>
--- nvim/nvim-raw.c
+++ nvim/nvim-raw.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <errno.h>
 #include <inttypes.h>
 #include <netinet/in.h>
@@ -11,38 +12,139 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-static int read_all(int fd, uint8_t* buf, uint64_t cap, uint64_t* len) {
-    memset(buf, 0, cap);
-    *len = 0;
+#define GB_IMPLEMENTATION
+#include "../vendor/gb/gb.h"
 
-    while (*len < cap) {
-        int ret = recv(fd, &buf[*len], cap - *len, 0);
+static int read_all(int fd, gbString* in) {
+    while (gb_string_available_space(*in) > 0) {
+        int ret = read(fd, *in + gb_string_length(*in),
+                       gb_string_available_space(*in));
         if (ret == -1) {
-            fprintf(stderr, "Failed to read from socket: %d %s", errno,
+            fprintf(stderr, "Failed to read from socket: %d %s\n", errno,
                     strerror(errno));
             return errno;
         }
         if (ret == 0) return 0;
 
-        for (uint16_t i = *len; i < *len + ret; i++) {
-            printf("%#0x ", buf[i]);
-        }
-        *len += ret;
+        gb__set_string_length(*in, gb_string_length(*in) + ret);
     }
 
     return 0;
 }
 
-static void handle_connection(int conn_fd) {
-    static uint8_t buf[MAXPATHLEN];
-    uint64_t len = 0;
+static void open_url_in_browser(gbString url) {
+    gbString cmd =
+        gb_string_make_reserve(gb_heap_allocator(), 10 + gb_string_length(url));
+    cmd = gb_string_append_fmt(cmd, "open '%s'", url);
+    printf("Running: %s\n", cmd);
+    FILE* cmd_handle = popen(cmd, "r");
+    assert(cmd_handle != NULL);
+
+    gb_string_free(cmd);
+}
+
+static void copy_to_clipboard(gbString s) {
+    gbString cmd =
+        gb_string_make_reserve(gb_heap_allocator(), 30 + gb_string_length(s));
+    cmd = gb_string_append_fmt(cmd, "printf '%s' | pbcopy", s);
+    printf("Running: %s\n", cmd);
+    FILE* cmd_handle = popen(cmd, "r");
+    assert(cmd_handle != NULL);
+
+    gb_string_free(cmd);
+}
+
+static gbString get_path_from_git_root() {
+    gbString cmd =
+        gb_string_make(gb_heap_allocator(), "git rev-parse --show-prefix");
+    printf("Running: %s\n", cmd);
+    FILE* cmd_handle = popen(cmd, "r");
+    assert(cmd_handle != NULL);
+
+    gb_string_free(cmd);
+
+    gbString output = gb_string_make_reserve(gb_heap_allocator(), MAXPATHLEN);
 
     int ret = 0;
-    if ((ret = read_all(conn_fd, buf, sizeof(buf), &len)) != 0) {
+    if ((ret = read_all(fileno(cmd_handle), &output)) != 0) {
+        fprintf(stderr, "Failed to read(2) output from command: %d %s\n", errno,
+                strerror(errno));
+        exit(errno);
+    }
+
+    output = gb_string_trim(output, "\n");
+
+    return output;
+}
+
+static void handle_connection(int conn_fd) {
+    gbString in = gb_string_make_reserve(gb_heap_allocator(), MAXPATHLEN);
+
+    int ret = 0;
+    if ((ret = read_all(conn_fd, &in)) != 0) {
         fprintf(stderr, "Failed to recv(2): %d %s\n", errno, strerror(errno));
         exit(errno);
     }
-    printf("\nRead: len=%llu\n", len);
+    printf("Received: %s\n", in);
+
+    char* branch = "master";
+    char* in_file_path_end =
+        memchr(in, 0xa /* newline */, gb_string_length(in));
+    assert(in_file_path_end != NULL);
+    gbString file_path = gb_string_make_reserve(gb_heap_allocator(), 20);
+    {
+        uint64_t in_file_path_len = in_file_path_end - (char*)in;
+        file_path = gb_string_append_length(file_path, in, in_file_path_len);
+    }
+    gbString remote_path = gb_string_make_reserve(gb_heap_allocator(), 20);
+    {
+        const char* sep = gb_char_last_occurence(file_path, '/');
+        assert(sep != NULL);
+        gbString dir = gb_string_make_length(gb_heap_allocator(), file_path,
+                                             sep - file_path);
+
+        if ((ret = chdir(dir)) != 0) {
+            fprintf(stderr, "Failed to chdir(2): file_path=%s errno=%d %s\n",
+                    dir, errno, strerror(errno));
+            exit(errno);
+        }
+
+        FILE* git_cmd_handle = popen("git remote get-url origin", "r");
+        gbString git_repository_url =
+            gb_string_make_reserve(gb_heap_allocator(), MAXPATHLEN);
+        if ((ret =
+                 read_all(fileno(git_cmd_handle), &git_repository_url) != 0)) {
+            fprintf(stderr, "Failed to read output from command: %d %s\n",
+                    errno, strerror(errno));
+            exit(errno);
+        }
+        printf("git_repository_url=%s\n", git_repository_url);
+
+        const char* remote_path_start =
+            gb_char_first_occurence(git_repository_url, ':');
+        assert(remote_path_start != NULL);
+        remote_path_start += 1;
+
+        assert(gb_str_has_suffix(remote_path_start, ".git\n"));
+
+        remote_path = gb_string_append_length(
+            remote_path, remote_path_start,
+            gb_string_length(git_repository_url) - 5 -
+                (remote_path_start - git_repository_url));
+    }
+    uint64_t line = strtoul(in_file_path_end + 1, NULL, 10);
+
+    gbString path_from_git_root = get_path_from_git_root();
+
+    gbString res_url = gb_string_make_reserve(gb_heap_allocator(), 100);
+    res_url = gb_string_append_fmt(
+        res_url, "https://gitlab.ppro.com/%s/-/tree/%s/%s%s#L%llu", remote_path,
+        branch, path_from_git_root, gb_path_base_name(file_path), line);
+    printf("Url: %s\n", res_url);
+
+    open_url_in_browser(res_url);
+
+    copy_to_clipboard(res_url);
 }
 
 int main() {
