d0540d394f70c6db73c2aad6c451e000f44435ba
Philippe Gaultier <philippe.gaultier@ppro.com>
2022-10-05 10:53:22 +0200

    impl hello world

--- nvim/nvim.c
+++ nvim/nvim.c
@@ -1,20 +1,29 @@
-#include <_types/_uint16_t.h>
+#include <_types/_uint32_t.h>
+#include <_types/_uint64_t.h>
 #include <errno.h>
 #include <inttypes.h>
 #include <netinet/in.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/errno.h>
 #include <sys/socket.h>
 #include <unistd.h>
+
+#include "vendor/msgpack-c/include/msgpack.h"
 
 static void handle_connection(int conn_fd) {
     static uint8_t buf[512];
     memset(buf, 0, sizeof(buf));
 
     uint16_t buf_len = 0;
-    while (true) {
+
+    msgpack_unpacked msg;
+    msgpack_unpacked_init(&msg);
+    msgpack_object obj = {0};
+    while (1) {
         int err = read(conn_fd, &buf[buf_len], sizeof(buf) - buf_len);
         if (err == -1) {
             fprintf(stderr, "Failed to read from socket: %d %s", errno,
@@ -27,9 +36,74 @@ static void handle_connection(int conn_fd) {
             printf("%#0x ", buf[i]);
         }
         buf_len += err;
+
+        msgpack_unpack_return ret =
+            msgpack_unpack_next(&msg, (const char*)buf, buf_len, NULL);
+        printf("msgpack: ret=%d\n", ret);
+        if (ret != MSGPACK_UNPACK_SUCCESS) {
+            continue;  // Need more data
+        }
+        obj = msg.data;
+        msgpack_object_print(stdout, obj);
+        break;
+    }
+    printf("\nRead: len=%d\n", buf_len);
+
+    if (obj.type != MSGPACK_OBJECT_ARRAY) {
+        fprintf(stderr, "Malformed requests, expected array, got: %d\n",
+                obj.type);
+        exit(EINVAL);
+    }
+    if (obj.via.array.size != 4) {
+        fprintf(stderr, "Malformed requests, expected 4 elements, got: %d\n",
+                obj.via.array.size);
+        exit(EINVAL);
     }
 
-    printf("\nRead: len=%d\n", buf_len);
+    msgpack_object msg_id = obj.via.array.ptr[1];
+    if (msg_id.type != MSGPACK_OBJECT_POSITIVE_INTEGER ||
+        msg_id.via.u64 > UINT32_MAX) {
+        fprintf(stderr, "Invalid msg id, expected uint32_t, got:");
+        msgpack_object_print(stderr, msg_id);
+        exit(EINVAL);
+    }
+    const uint32_t msg_id_u32 = msg_id.via.u64;
+
+    // Response
+    msgpack_sbuffer* buffer = msgpack_sbuffer_new();
+    msgpack_packer* pk = msgpack_packer_new(buffer, msgpack_sbuffer_write);
+    msgpack_pack_array(pk, 4);
+    msgpack_pack_int(pk, 1);           // RESPONSE
+    msgpack_pack_int(pk, msg_id_u32);  // MSG ID
+    msgpack_pack_nil(pk);              // ERROR
+    msgpack_pack_array(pk, 2);         // RESULT
+    msgpack_pack_int(pk, msg_id_u32);
+    msgpack_pack_nil(pk);
+
+    uint64_t sent = 0;
+    printf("Sending: ");
+    while (sent < buffer->size) {
+        int ret = send(conn_fd, buffer->data + sent, buffer->size - sent, 0);
+        if (ret == -1) {
+            fprintf(stderr, "Failed to send(2): %d %s\n", errno,
+                    strerror(errno));
+            exit(errno);
+        }
+        if (ret == 0) {
+            fprintf(stderr, "Other side closed\n");
+            exit(0);
+        }
+        for (uint64_t i = sent; i < sent + ret; i++) {
+            printf("%02x ", buffer->data[i]);
+        }
+        sent += ret;
+    }
+
+    msgpack_sbuffer_free(buffer);
+    msgpack_packer_free(pk);
+    msgpack_unpacked_destroy(&msg);
+
+    sleep(99);  // FIXME
 }
 
 int main() {
