3bbb164334629b5a4fe18cd934ca5026ef40c4bf
Philippe Gaultier <philippe.gaultier@ppro.com>
2022-10-06 15:41:03 +0200

    add shift

--- on-call/main.c
+++ on-call/main.c
@@ -160,6 +160,7 @@ int main() {
         {DATE_TIME(2022, 9, 12, 18), DATE_TIME(2022, 9, 14, 9)},
         {DATE_TIME(2022, 9, 26, 18), DATE_TIME(2022, 9, 27, 9)},
         {DATE_TIME(2022, 9, 28, 18), DATE_TIME(2022, 10, 3, 9)},
+        {DATE_TIME(2022, 10, 24, 18), DATE_TIME(2022, 10, 28, 9)},
     };
     bill_shifts(shifts, sizeof(shifts) / sizeof(shifts[0]));
 }
