b728d7ccab5b98fc3c799b0aee6e8ad539864caf
Philippe Gaultier <philippe.gaultier@ppro.com>
2022-10-07 09:04:31 +0200

    static alloc

--- nvim/gitlab-url-copy.c
+++ nvim/gitlab-url-copy.c
@@ -18,6 +18,7 @@
 
 #define MAX_URL_LEN 2048
 #define GIT_COMMIT_LENGTH (2 * 20)
+#define MEM_SIZE (5 * MAX_URL_LEN)
 
 static int read_all(int fd, gbString* in) {
     while (gb_string_available_space(*in) > 0) {
@@ -36,9 +37,9 @@ static int read_all(int fd, gbString* in) {
     return 0;
 }
 
-static void open_url_in_browser(gbString url) {
+static void open_url_in_browser(gbAllocator allocator, gbString url) {
     gbString cmd = gb_string_make_reserve(
-        gb_heap_allocator(), sizeof("open ''") + gb_string_length(url));
+        allocator, sizeof("open ''") + gb_string_length(url));
     cmd = gb_string_append_fmt(cmd, "open '%s'", url);
     printf("Running: %s\n", cmd);
     FILE* cmd_handle = popen(cmd, "r");
@@ -47,10 +48,9 @@ static void open_url_in_browser(gbString url) {
     gb_string_free(cmd);
 }
 
-static void copy_to_clipboard(gbString s) {
+static void copy_to_clipboard(gbAllocator allocator, gbString s) {
     gbString cmd = gb_string_make_reserve(
-        gb_heap_allocator(),
-        sizeof("printf '' | pbcopy") + gb_string_length(s));
+        allocator, sizeof("printf '' | pbcopy") + gb_string_length(s));
     cmd = gb_string_append_fmt(cmd, "printf '%s' | pbcopy", s);
     printf("Running: %s\n", cmd);
     FILE* cmd_handle = popen(cmd, "r");
@@ -59,13 +59,13 @@ static void copy_to_clipboard(gbString s) {
     gb_string_free(cmd);
 }
 
-static gbString get_path_from_git_root() {
+static gbString get_path_from_git_root(gbAllocator allocator) {
     const char* const cmd = "git rev-parse --show-prefix";
     printf("Running: %s\n", cmd);
     FILE* cmd_handle = popen(cmd, "r");
     assert(cmd_handle != NULL);
 
-    gbString output = gb_string_make_reserve(gb_heap_allocator(), MAXPATHLEN);
+    gbString output = gb_string_make_reserve(allocator, MAXPATHLEN);
 
     int ret = 0;
     if ((ret = read_all(fileno(cmd_handle), &output)) != 0) {
@@ -79,14 +79,13 @@ static gbString get_path_from_git_root() {
     return output;
 }
 
-static gbString get_current_git_commit() {
+static gbString get_current_git_commit(gbAllocator allocator) {
     const char* const cmd = "git rev-parse HEAD";
     printf("Running: %s\n", cmd);
     FILE* cmd_handle = popen(cmd, "r");
     assert(cmd_handle != NULL);
 
-    gbString output =
-        gb_string_make_reserve(gb_heap_allocator(), GIT_COMMIT_LENGTH);
+    gbString output = gb_string_make_reserve(allocator, GIT_COMMIT_LENGTH);
 
     int ret = 0;
     if ((ret = read_all(fileno(cmd_handle), &output)) != 0) {
@@ -98,13 +97,13 @@ static gbString get_current_git_commit() {
     return output;
 }
 
-static gbString get_git_origin_remote_url() {
+static gbString get_git_origin_remote_url(gbAllocator allocator) {
     char* cmd = "git remote get-url origin";
     printf("Running: %s\n", cmd);
     FILE* cmd_handle = popen(cmd, "r");
     assert(cmd_handle != NULL);
 
-    gbString output = gb_string_make_reserve(gb_heap_allocator(), MAX_URL_LEN);
+    gbString output = gb_string_make_reserve(allocator, MAX_URL_LEN);
 
     int ret = 0;
     if ((ret = read_all(fileno(cmd_handle), &output)) != 0) {
@@ -118,16 +117,16 @@ static gbString get_git_origin_remote_url() {
     return output;
 }
 
-static gbString path_get_directory(gbString path) {
+static gbString path_get_directory(gbAllocator allocator, gbString path) {
     const char* sep = gb_char_last_occurence(path, '/');
     assert(sep != NULL);
-    gbString dir = gb_string_make_length(gb_heap_allocator(), path, sep - path);
+    gbString dir = gb_string_make_length(allocator, path, sep - path);
 
     return dir;
 }
 
 static gbString get_project_path_from_remote_git_url(
-    gbString git_repository_url) {
+    gbAllocator allocator, gbString git_repository_url) {
     const char* remote_path_start =
         gb_char_first_occurence(git_repository_url, ':');
     assert(remote_path_start != NULL);
@@ -139,15 +138,20 @@ static gbString get_project_path_from_remote_git_url(
                          (sizeof(".git") - 1) -
                          (remote_path_start - git_repository_url);
     gbString project_path =
-        gb_string_make_length(gb_heap_allocator(), remote_path_start, len);
+        gb_string_make_length(allocator, remote_path_start, len);
     return project_path;
 }
 
 int main(int argc, char* argv[]) {
     assert(argc == 3);
 
-    gbString file_path = gb_string_make(gb_heap_allocator(), argv[1]);
-    gbString dir = path_get_directory(file_path);
+    static char mem[MEM_SIZE] = {};
+    gbArena arena = {0};
+    gb_arena_init_from_memory(&arena, mem, MEM_SIZE);
+    gbAllocator allocator = gb_arena_allocator(&arena);
+
+    gbString file_path = gb_string_make(allocator, argv[1]);
+    gbString dir = path_get_directory(allocator, file_path);
 
     int ret = 0;
     if ((ret = chdir(dir)) != 0) {
@@ -156,24 +160,24 @@ int main(int argc, char* argv[]) {
         exit(errno);
     }
 
-    gbString git_repository_url = get_git_origin_remote_url();
+    gbString git_repository_url = get_git_origin_remote_url(allocator);
     printf("git_repository_url=%s\n", git_repository_url);
 
     gbString project_path =
-        get_project_path_from_remote_git_url(git_repository_url);
+        get_project_path_from_remote_git_url(allocator, git_repository_url);
 
-    gbString path_from_git_root = get_path_from_git_root();
-    gbString commit = get_current_git_commit();
+    gbString path_from_git_root = get_path_from_git_root(allocator);
+    gbString commit = get_current_git_commit(allocator);
     const uint64_t line = strtoul(argv[2], NULL, 10);
 
-    gbString res_url = gb_string_make_reserve(gb_heap_allocator(), MAX_URL_LEN);
+    gbString res_url = gb_string_make_reserve(allocator, MAX_URL_LEN);
     res_url = gb_string_append_fmt(
         res_url, "https://gitlab.ppro.com/%s/-/blob/%s/%s%s#L%llu",
         project_path, commit, path_from_git_root, gb_path_base_name(file_path),
         line);
     printf("Url: %s\n", res_url);
 
-    open_url_in_browser(res_url);
+    open_url_in_browser(allocator, res_url);
 
-    copy_to_clipboard(res_url);
+    copy_to_clipboard(allocator, res_url);
 }
