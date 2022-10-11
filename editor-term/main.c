5feb0bfa7b7005942a2e77e62ec08ce9675788dd
Philippe Gaultier <philippe.gaultier@ppro.com>
2022-10-11 08:02:37 +0200

    expand editor

--- editor-term/main.c
+++ editor-term/main.c
@@ -1,5 +1,6 @@
 #include <assert.h>
 #include <errno.h>
+#include <malloc/_malloc.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,13 +9,95 @@
 #include <termios.h>
 #include <unistd.h>
 
+#define CLAMP(min, n, max) \
+  do {                     \
+    if ((n) < (min))       \
+      (n) = (min);         \
+    else if ((n) > (max))  \
+      (n) = (max);         \
+  } while (0)
+
 static struct termios original_termios;
 
-static void disable_raw_mode() {
+typedef enum {
+  K_NONE = 0,
+  K_TAB = 9,
+  K_ENTER = 13,
+  K_ESC = 27,
+} key_t;
+
+typedef struct {
+  char* s;
+  int cap, len;
+} buf_t;
+
+typedef struct {
+  // Screen dimensions
+  int rows, cols;
+  // Cursor
+  int cx, cy, coffset;
+  buf_t draw_buf;
+} editor_t;
+
+static buf_t buf_make(int cap) {
+  buf_t buf = {
+      .s = calloc(cap, 1),
+      .cap = cap,
+  };
+  return buf;
+}
+
+static void buf_append(buf_t* buf, char* s, int size) {
+  assert(buf->len + size < buf->cap);
+
+  memcpy(buf->s + buf->len, s, size);
+  buf->len += size;
+}
+
+static void buf_reset(buf_t* buf) { buf->len = 0; }
+
+static key_t read_key() {
+  char c = 0;
+  if (read(STDIN_FILENO, &c, 1) < 0) {
+    fprintf(stderr, "Failed to read(2): %s\n", strerror(errno));
+    exit(errno);
+  }
+  return c;
+}
+
+static void handle_key(editor_t* editor, key_t key) {
+  switch ((int)key) {
+    case K_ESC:
+      exit(0);
+      break;
+    case 'h':
+      editor->cx--;
+      CLAMP(0, editor->cx, editor->cols);
+      break;
+    case 'j':
+      editor->cy++;
+      CLAMP(0, editor->cy, editor->rows);
+      break;
+    case 'k':
+      editor->cy--;
+      CLAMP(0, editor->cy, editor->rows);
+      break;
+    case 'l':
+      editor->cx++;
+      CLAMP(0, editor->cx, editor->cols);
+      break;
+    default:
+      break;
+  }
+}
+
+static void screen_disable_raw_mode_and_reset() {
+  write(STDOUT_FILENO, "\x1b[0m\x1b[J\x1b[H",
+        10);  // Reset, Clear screen, Go home
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_termios);
 }
 
-static void enable_raw_mode() {
+static void screen_enable_raw_mode() {
   if (tcgetattr(STDIN_FILENO, &original_termios) == -1) {
     fprintf(stderr, "tcgetattr failed: %s\n", strerror(errno));
     exit(errno);
@@ -40,10 +123,10 @@ static void enable_raw_mode() {
     fprintf(stderr, "tcgetattr failed: %s\n", strerror(errno));
     exit(errno);
   }
-  atexit(disable_raw_mode);
+  atexit(screen_disable_raw_mode_and_reset);
 }
 
-static int get_window_size(int* cols, int* rows) {
+static void get_window_size(int* cols, int* rows) {
   struct winsize ws = {0};
   if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1) {
     fprintf(stderr, "ioctl(2) failed: %s\n", strerror(errno));
@@ -51,45 +134,47 @@ static int get_window_size(int* cols, int* rows) {
   }
   *cols = ws.ws_col;
   *rows = ws.ws_row;
-
-  return 0;
 }
 
-#define BUF_CAP (20 * 1024)
-static char buf[BUF_CAP];
-static int buf_len = 0;
+static void draw(editor_t* e) {
+  assert(e->rows > 0);
+  assert(e->cols > 0);
 
-static void buf_append(char* s, int size) {
-  assert(buf_len + size < BUF_CAP);
+  buf_append(&e->draw_buf, "\x1b[J", 1);     // Clear screen
+  buf_append(&e->draw_buf, "\x1b[?25l", 6);  // Hide cursor
+  buf_append(&e->draw_buf, "\x1b[H", 3);     // Go home
 
-  memcpy(buf + buf_len, s, size);
-  buf_len += size;
+  char debug[80] = "";
+  int debug_len = snprintf(debug, sizeof(debug) - 1,
+                           "\x1b[0Kcols=%d rows=%d cx=%d cy=%d\r\n", e->cols,
+                           e->rows, e->cx, e->cy);
+  buf_append(&e->draw_buf, debug, debug_len);
+  // Padding
+  buf_append(&e->draw_buf, "\x1b[41m", 5);
+  for (int i = 1; i < e->rows - 1; i++) {
+    buf_append(&e->draw_buf, "\x1b[0K", 4);
+    buf_append(&e->draw_buf, "\r\n", 2);
+  }
+  buf_append(&e->draw_buf, "\x1b[?25h", 6);  // Show cursor
+
+  write(STDOUT_FILENO, e->draw_buf.s, e->draw_buf.len);
 }
 
 int main() {
-  enable_raw_mode();
+  screen_enable_raw_mode();
   int cols = 0, rows = 0;
   get_window_size(&cols, &rows);
-  /* printf("%d %d\n", cols, rows); */
+  editor_t editor = {
+      .draw_buf = buf_make(cols * rows * 5), .cols = cols, .rows = rows};
 
-  buf_append("\x1b[J", 1);     // Clear screen
-  buf_append("\x1b[?25l", 6);  // Hide cursor
-  buf_append("\x1b[H", 3);     // Go home
-
-  buf_append("\x1b[44m\x1b[0Khello\x1b[44m\r\n", 21);
-  // Padding
-  buf_append("\x1b[41m", 5);
-  for (int i = 1; i < rows - 1; i++) {
-    buf_append("\x1b[0K", 4);
-    for (int j = 0; j < cols; j++) {
-      /* buf_append(" ", 1); */
+  while (1) {
+    const key_t key = read_key();
+    if (key == 0) {
+      usleep(50);
     }
-    buf_append("\r\n", 2);
+
+    handle_key(&editor, key);
+    draw(&editor);
+    buf_reset(&editor.draw_buf);
   }
-  /* \x1b[0m */
-  /* char buf[] = ; */
-
-  buf_append("\x1b[?25h", 6);  // Show cursor
-
-  write(STDOUT_FILENO, buf, buf_len);
 }
