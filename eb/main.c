275144fb77acd0767cf5200cadbd5c5b2e6ff9fe
Philippe Gaultier <philippe.gaultier@ppro.com>
2022-10-12 14:31:24 +0200

    use posix_spawnp

--- eb/main.c
+++ eb/main.c
@@ -1,8 +1,10 @@
 #include <errno.h>
+#include <spawn.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/errno.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 #define GB_IMPLEMENTATION
@@ -22,15 +24,28 @@ int main(int argc, char* argv[]) {
     uint64_t sleep_milliseconds = 100;
     while (1) {
         puts(cmd);
-        int ret = system(cmd);
-        if (ret == 127) {
+        pid_t pid = {0};
+        int ret = posix_spawnp(&pid, argv[1], NULL, NULL, argv + 2, NULL);
+        if (ret < 0) {
             fprintf(stderr, "Failed to run command (malformed?): %s",
                     strerror(errno));
-            return errno > 0 ? errno : EINVAL;
+            return errno;
         }
-        if (ret == 0) return 0;
 
-        usleep(sleep_milliseconds);
-        sleep_milliseconds *= 1.5;  // TODO: jitter, random
+        int status = 0;
+        if (wait(&status) == -1) {
+            fprintf(stderr, "Failed wait(2): %s", strerror(errno));
+            return errno;
+        }
+        if (WIFEXITED(status)) {
+            if (WEXITSTATUS(status) == 0)
+                return 0;
+            else {
+                usleep(sleep_milliseconds);
+                sleep_milliseconds *= 1.5;  // TODO: jitter, random
+                continue;
+            }
+        }
+        // TODO
     }
 }
