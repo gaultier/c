235b94d1c9bf60ad1c9516cff7c56830be0c36de
Philippe Gaultier <philippe.gaultier@ppro.com>
2022-10-12 16:43:13 +0200

    add options

--- eb/main.c
+++ eb/main.c
@@ -1,5 +1,8 @@
 #include <errno.h>
+#include <getopt.h>
+#include <inttypes.h>
 #include <spawn.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,20 +10,80 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+typedef struct {
+    uint64_t max_retries, max_duration_seconds, wait_milliseconds;
+} options_t;
+
+static void print_usage(char* argv0) {
+    printf(
+        "%s\n\t--max-retries, -r\tHow many maximum retries to "
+        "attempt.\n\t--max-duration-seconds, -d\tHow long to "
+        "run.\n\t--wait-ms, -w\tHow much time to wait between invocations of "
+        "the program.\n\t--help, -h\tThis help message.\n",
+        argv0);
+}
+
+static void options_parse_from_cli(int argc, char* argv[], options_t* options) {
+    struct option longopts[] = {
+        {.name = "max-retries",
+         .has_arg = required_argument,
+         .flag = NULL,
+         .val = 'r'},
+        {.name = "max-duration-seconds",
+         .has_arg = required_argument,
+         .flag = NULL,
+         .val = 'd'},
+        {.name = "wait-ms",
+         .has_arg = required_argument,
+         .flag = NULL,
+         .val = 'w'},
+        {.name = "help", .has_arg = no_argument, .flag = NULL, .val = 'h'},
+    };
+
+    int ch = 0;
+    while ((ch = getopt_long(argc, argv, "hr:d:w:", longopts, NULL)) != -1) {
+        switch (ch) {
+            case 'r': {
+                options->max_retries = strtoull(optarg, NULL, 10);
+                break;
+            }
+            case 'd': {
+                options->max_duration_seconds = strtoull(optarg, NULL, 10);
+                break;
+            }
+            case 'w': {
+                options->wait_milliseconds = strtoull(optarg, NULL, 10);
+                break;
+            }
+            default:
+                print_usage(argv[0]);
+                exit(0);
+        }
+    }
+}
+
 int main(int argc, char* argv[], char* envp[]) {
     if (argc == 1) {
         return EINVAL;
     }
+    options_t options = {.max_retries = UINT64_MAX};
+    options_parse_from_cli(argc, argv, &options);
+    argc -= optind;
+    argv += optind;
+
+    if (options.max_duration_seconds > 0) {
+        // TODO
+    }
 
     uint64_t sleep_milliseconds = 100;
-    while (1) {
-        for (int i = 1; i < argc; i++) {
+    for (uint64_t attempt = 0; attempt < options.max_retries; attempt++) {
+        for (int i = 0; i < argc; i++) {
             printf("%s ", argv[i]);
         }
         printf("\n");
 
         pid_t pid = {0};
-        int ret = posix_spawnp(&pid, argv[1], NULL, NULL, argv + 1, envp);
+        int ret = posix_spawnp(&pid, argv[0], NULL, NULL, argv, envp);
         if (ret < 0) {
             fprintf(stderr, "Failed to run command (malformed?): %s",
                     strerror(errno));
@@ -36,7 +99,9 @@ int main(int argc, char* argv[], char* envp[]) {
             if (WEXITSTATUS(status) == 0)
                 return 0;
             else {
-                usleep(sleep_milliseconds);
+                usleep(1000 * (options.wait_milliseconds > 0
+                                   ? options.wait_milliseconds
+                                   : sleep_milliseconds));
                 sleep_milliseconds *= 1.5;  // TODO: jitter, random
                 continue;
             }
