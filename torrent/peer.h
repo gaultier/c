a1282d9c99a4c377c2c1a1d1d1f8295a5aaab3fa
Philippe Gaultier <philippe.gaultier@ppro.com>
2022-11-02 12:12:17 +0100

    refactor

--- torrent/peer.h
+++ torrent/peer.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <arpa/inet.h>
+#include <netinet/in.h>
 #include <sys/socket.h>
 #include <uv.h>
 
@@ -26,7 +28,8 @@ typedef struct {
   uv_tcp_t connection;
   uv_connect_t connect_req;
 
-  char addr[14];
+  struct sockaddr_in addr;
+  char addr_s[INET_ADDRSTRLEN];  // TODO: INET6_ADDRSTRLEN
 } peer_t;
 
 void peer_on_connect(uv_connect_t* handle, int status) {
@@ -34,41 +37,44 @@ void peer_on_connect(uv_connect_t* handle, int status) {
   assert(peer != NULL);
 
   if (status != 0) {
-    fprintf(stderr, "[%s] on_connect: %d %s\n", peer->addr, status,
-            strerror(status));
+    fprintf(stderr, "[%s] on_connect: %d %s\n", peer->addr_s, -status,
+            strerror(-status));
     // TODO remove_peer_connection
     return;
   }
-  fprintf(stderr, "[%s] Connected\n", peer->addr);
+  fprintf(stderr, "[%s] Connected\n", peer->addr_s);
 }
 
-peer_t* peer_make(pg_allocator_t allocator, bc_metainfo_t* metainfo) {
+peer_t* peer_make(pg_allocator_t allocator, bc_metainfo_t* metainfo,
+                  const tracker_peer_address_t* address) {
   peer_t* peer = allocator.realloc(sizeof(peer_t), NULL, 0);
   peer->metainfo = metainfo;
   peer->connect_req.data = peer;
   peer->connection.data = peer;
-  return peer;
-}
-
-peer_error_t peer_connect(peer_t* peer, const tracker_peer_address_t* address) {
-  int ret = 0;
-  if ((ret = uv_tcp_init(uv_default_loop(), &peer->connection)) != 0) {
-    fprintf(stderr, "[%s] Failed to uv_tcp_init: %d %s\n", peer->addr, ret,
-            strerror(ret));
-    return (peer_error_t){.kind = PEK_UV, .v = {.uv_err = ret}};
-  }
-
-  struct sockaddr_in addr = {
+  peer->addr = (struct sockaddr_in){
       .sin_port = address->port,
       .sin_family = AF_INET,
       .sin_addr = {.s_addr = address->ip},
   };
+  assert(inet_ntop(AF_INET, &peer->addr, peer->addr_s, sizeof(peer->addr_s)) !=
+         NULL);
+  return peer;
+}
+
+peer_error_t peer_connect(peer_t* peer) {
+  int ret = 0;
+  if ((ret = uv_tcp_init(uv_default_loop(), &peer->connection)) != 0) {
+    fprintf(stderr, "[%s] Failed to uv_tcp_init: %d %s\n", peer->addr_s, ret,
+            strerror(ret));
+    return (peer_error_t){.kind = PEK_UV, .v = {.uv_err = -ret}};
+  }
 
   if ((ret = uv_tcp_connect(&peer->connect_req, &peer->connection,
-                            (struct sockaddr*)&addr, peer_on_connect)) != 0) {
-    fprintf(stderr, "[%s] Failed to uv_tcp_connect: %d %s\n", peer->addr, ret,
+                            (struct sockaddr*)&peer->addr, peer_on_connect)) !=
+      0) {
+    fprintf(stderr, "[%s] Failed to uv_tcp_connect: %d %s\n", peer->addr_s, ret,
             strerror(ret));
-    return (peer_error_t){.kind = PEK_UV, .v = {.uv_err = ret}};
+    return (peer_error_t){.kind = PEK_UV, .v = {.uv_err = -ret}};
   }
   return (peer_error_t){.kind = PEK_NONE};
 }
