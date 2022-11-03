5d79fae4b4b09ff53d6ced510569f13a405422ab
Philippe Gaultier <philippe.gaultier@ppro.com>
2022-11-03 09:09:59 +0100

    add pg_ring_t

--- pg/pg.h
+++ pg/pg.h
@@ -15,6 +15,10 @@
 #define MIN(a, b) (((a) < (b)) ? (a) : (b))
 #endif
 
+#ifndef MAX
+#define MAX(a, b) (((a) > (b)) ? (a) : (b))
+#endif
+
 typedef struct {
   char *data;
   uint64_t len;
@@ -496,3 +500,109 @@ typedef struct {
     }                                                             \
   } while (0)
 
+// -------------------------- Ring buffer of bytes
+
+typedef struct {
+  pg_array_t(uint8_t) data;
+  uint64_t len, offset;
+} pg_ring_t;
+
+void pg_ring_init(pg_allocator_t allocator, pg_ring_t *ring, uint64_t cap) {
+  ring->len = ring->offset = 0;
+  pg_array_init_reserve(ring->data, cap, allocator);
+}
+
+void pg_ring_destroy(pg_ring_t *ring) { pg_array_free(ring->data); }
+
+uint8_t *pg_ring_get_ptr(pg_ring_t *ring, uint64_t i) {
+  assert(i < pg_array_count(ring->data));
+  const uint64_t index = (i + ring->offset) % pg_array_count(ring->data);
+  return &ring->data[index];
+}
+
+uint8_t pg_ring_get(pg_ring_t *ring, uint64_t i) {
+  return *pg_ring_get_ptr(ring, i);
+}
+
+uint8_t *pg_ring_front_ptr(pg_ring_t *ring) {
+  return &ring->data[ring->offset];
+}
+
+uint8_t pg_ring_front(pg_ring_t *ring) { return *pg_ring_front_ptr(ring); }
+
+uint8_t *pg_ring_back_ptr(pg_ring_t *ring) {
+  const uint64_t index =
+      (ring->offset + ring->len) % pg_array_count(ring->data);
+  return &ring->data[index];
+}
+
+uint8_t pg_ring_back(pg_ring_t *ring) { return *pg_ring_back_ptr(ring); }
+
+void pg_ring_grow(pg_ring_t *ring, uint64_t min_cap) {
+  const uint64_t n = pg_array_count(ring->data);
+  const uint64_t new_cap = MAX(MAX(min_cap, 8), n * 1.5);
+  pg_array_grow(ring->data, new_cap);
+
+  // TODO: improve
+  if (ring->len < ring->offset) {
+    memmove(ring->data, ring->data + ring->offset, ring->len);
+    ring->offset = 0;
+  }
+}
+
+void pg_ring_push_back(pg_ring_t *ring, uint8_t x) {
+  if (pg_array_available_space(ring) == 0) {
+    pg_array_grow(ring, 0);
+  }
+
+  const uint64_t index =
+      (ring->offset + ring->len) % pg_array_count(ring->data);
+  ring->data[index] = x;
+  ring->len += 1;
+}
+
+void pg_ring_push_front(pg_ring_t *ring, uint8_t x) {
+  if (pg_array_available_space(ring) == 0) {
+    pg_array_grow(ring, 0);
+  }
+
+  ring->offset = (ring->offset - 1 + pg_array_count(ring->data)) %
+                 pg_array_count(ring->data);
+  ring->data[ring->offset] = x;
+  ring->len += 1;
+}
+
+uint8_t pg_ring_pop_back(pg_ring_t *ring) {
+  assert(ring->len > 0);
+  ring->len -= 1;
+  const uint64_t index =
+      (ring->offset + ring->len) % pg_array_count(ring->data);
+  return ring->data[index];
+}
+
+uint8_t pg_ring_pop_front(pg_ring_t *ring) {
+  assert(ring->len > 0);
+  const uint8_t res = ring->data[ring->offset];
+
+  ring->offset = (ring->offset + 1) % pg_array_count(ring->data);
+  ring->len -= 1;
+
+  return res;
+}
+
+void pg_ring_consume_front(pg_ring_t *ring, uint64_t n) {
+  assert(n <= ring->len);
+  ring->offset = (ring->offset - n) % pg_array_count(ring->data);
+  ring->len -= n;
+}
+
+void pg_ring_consume_back(pg_ring_t *ring, uint64_t n) {
+  assert(n <= ring->len);
+  ring->len -= n;
+}
+
+void pg_ring_clear(pg_ring_t *ring) {
+  pg_array_clear(ring->data);
+  ring->offset = 0;
+  ring->len = 0;
+}
