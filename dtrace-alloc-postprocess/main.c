f3fe92eeb634d6217c8a8fc8d2ae0e3207b0418a
Philippe Gaultier <philippe.gaultier@ppro.com>
2022-11-14 10:22:58 +0100

    parse

--- dtrace-alloc-postprocess/main.c
+++ dtrace-alloc-postprocess/main.c
@@ -3,7 +3,7 @@
 #include <sys/fcntl.h>
 #include <unistd.h>
 
-#include "../pg//pg.h"
+#include "../pg/pg.h"
 
 int main(int argc, char* argv[]) {
   pg_logger_t logger = {.level = PG_LOG_INFO};
@@ -45,8 +45,50 @@ int main(int argc, char* argv[]) {
       pg_span_skip_left_until_inclusive(&input, ' ');
     }
 
+    // malloc/realloc/calloc/free
     pg_span_trim_left(&input);
     pg_span_t fn_leaf = input;
     pg_span_split(input, ' ', &fn_leaf, &input);
+    pg_span_trim_left(&input);
+
+    // timestamp
+    pg_span_t ts = {0};
+    pg_span_split(input, ' ', &ts, &input);
+    pg_span_trim_left(&input);
+
+    // arg0
+    pg_span_t arg0 = {0};
+    pg_span_split(input, ' ', &arg0, &input);
+    pg_span_trim_left(&input);
+
+    // arg1
+    {
+      bool more_chars = false;
+      char c = pg_span_peek_left(input, &more_chars);
+      if (!more_chars) break;
+      if (c != '\n') {
+        pg_span_t arg1 = {0};
+        pg_span_split(input, '\n', &arg1, &input);
+        pg_span_trim_left(&input);
+      }
+    }
+
+    // Rest of stacktrace
+    while (true) {
+      bool more_chars = false;
+      char c = pg_span_peek_left(input, &more_chars);
+      if (!more_chars) break;
+      if (c == '\n') {  // New frame
+        // TODO
+        return 1;
+      }
+
+      pg_span_trim_left(&input);
+      pg_span_t fn = input;
+      pg_span_split(input, ' ', &fn, &input);
+      pg_span_trim_left(&input);
+      pg_span_trim_right(&fn);
+    }
   }
+  return 0;
 }
