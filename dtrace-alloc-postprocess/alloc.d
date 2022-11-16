94dbd6c1ef010a2a642829ff6094bdb22cb90776
Philippe Gaultier <philippe.gaultier@ppro.com>
2022-11-16 19:06:46 +0100

    simplify dtrace scripts

--- dtrace-alloc-postprocess/alloc.d
+++ dtrace-alloc-postprocess/alloc.d
@@ -1,9 +1,15 @@
 #!/usr/sbin/dtrace -s
 
-pid$target::malloc:entry{printf("%u %u",timestamp, arg0); ustack();}
-pid$target::realloc:entry {printf("%u %p %u", timestamp, arg0, arg1); ustack();}
-pid$target::calloc:entry {printf("%u %u %u", timestamp, arg0, arg1); ustack();}
+pid$target::malloc:entry{ self->mem_size = arg0; }
+pid$target::realloc:entry { self->old_ptr = arg0; self->mem_size = arg1; }
+pid$target::calloc:entry { self->mem_size = arg0*arg1; }
 
-pid$target::malloc:return, pid$target::realloc:return, pid$target::calloc:return  {printf("%u %p", timestamp, arg1); ustack();}
+pid$target::malloc:return, pid$target::realloc:return, pid$target::calloc:return  {
+  printf("%u %u %p %p", timestamp, self->mem_size, arg1, self->old_ptr); 
+  ustack();
+  
+  self->mem_size = 0;
+  self->old_ptr = 0;
+}
 
 pid$target::free:entry /arg0!=0/ {printf("%u %p",timestamp, arg0); ustack();}
