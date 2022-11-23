77346e2bbfa26382f7a08fa51bc33434bc2afd9c
Philippe Gaultier <philippe.gaultier@ppro.com>
2022-11-23 12:24:56 +0100

    readd deleted functions by mistake

--- dtrace-alloc-postprocess/main.c
+++ dtrace-alloc-postprocess/main.c
@@ -1,4 +1,3 @@
-#include <_types/_uint64_t.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <stdlib.h>
@@ -505,7 +504,7 @@ int main(int argc, char* argv[]) {
       pg_log_fatal(&logger, errno, "Failed to open file %s: %s", argv[1],
                    strerror(errno));
     }
-    int64_t ret = 0;
+    int ret = 0;
     if ((ret = pg_read_file(pg_heap_allocator(), argv[1], &file_data)) != 0) {
       pg_log_fatal(&logger, ret, "Failed to read file %s: %s", argv[1],
                    strerror(ret));
--- pg/pg.h
+++ pg/pg.h
@@ -1030,3 +1030,36 @@ __attribute__((unused)) static void pg_bitarray_resize(pg_bitarray_t *bitarr,
   const uint64_t len = (uint64_t)(ceil(((double)max_index) / 8.0));
   pg_array_resize(bitarr->data, len);
 }
+
+// ------------- File utils
+
+int64_t pg_read_file_fd(pg_allocator_t allocator, int fd,
+                        pg_array_t(uint8_t) * buf) {
+  struct stat st = {0};
+  if (fstat(fd, &st) == -1) {
+    return errno;
+  }
+  const uint64_t read_buffer_size =
+      MIN((uint64_t)UINT32_MAX, (uint64_t)st.st_size);
+  pg_array_init_reserve(*buf, st.st_size, allocator);
+  while (pg_array_len(*buf) < (uint64_t)st.st_size) {
+    int64_t ret = read(fd, *buf + pg_array_len(*buf), read_buffer_size);
+    if (ret == -1) {
+      return errno;
+    }
+    if (ret == 0) return 0;
+    pg_array_resize(*buf, pg_array_len(*buf) + ret);
+  }
+  return 0;
+}
+
+int64_t pg_read_file(pg_allocator_t allocator, char *path,
+                     pg_array_t(uint8_t) * buf) {
+  int fd = open(path, O_RDONLY);
+  if (fd == -1) {
+    return errno;
+  }
+  int ret = pg_read_file_fd(allocator, fd, buf);
+  close(fd);
+  return ret;
+}
