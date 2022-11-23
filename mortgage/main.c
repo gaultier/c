6a5ed6e8bf09926665dfec206bdcaaa61754618c
Philippe Gaultier <philippe.gaultier@ppro.com>
2022-11-23 12:14:33 +0100

    fix many warnings

--- mortgage/main.c
+++ mortgage/main.c
@@ -1,24 +1,23 @@
+#include <_types/_uint64_t.h>
 #include <assert.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "../pg.h"
-
 int main(int argc, char* argv[]) {
     assert(argc == 4);
 
-    const float yearly_interest_rate = strtof(argv[1], NULL);
-    const float monthly_interest_rate = yearly_interest_rate / 12;
-    const u64 months_total = strtoull(argv[2], NULL, 10);
-    const float principal = strtoull(argv[3], NULL, 10);
+    const double yearly_interest_rate = strtod(argv[1], NULL);
+    const double monthly_interest_rate = yearly_interest_rate / 12;
+    const uint64_t months_total = strtoull(argv[2], NULL, 10);
+    const double principal = (double)(strtoull(argv[3], NULL, 10));
 
-    float paid = 0;
-    const float monthly_payment =
+    double paid = 0;
+    const double monthly_payment =
         principal * monthly_interest_rate *
-        powf(1 + monthly_interest_rate, months_total) /
-        (powf(1 + monthly_interest_rate, months_total) - 1);
-    for (u64 month = 1; month <= months_total; month++) {
+        pow(1 + monthly_interest_rate, (double)months_total) /
+        (pow(1 + monthly_interest_rate, (double)months_total) - 1);
+    for (uint64_t month = 1; month <= months_total; month++) {
         paid += monthly_payment;
     }
     assert(paid > principal);
