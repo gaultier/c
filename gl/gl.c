00be9804abee2e3fab64fc9d5908a9316cf442c1
Philippe Gaultier <philippe.gaultier@ppro.com>
2022-12-15 09:24:52 +0100

    extract

--- gl/gl.c
+++ gl/gl.c
@@ -1,15 +1,17 @@
 #include <OpenGL/gl3.h>
 #include <SDL2/SDL.h>
-#include <_types/_uint64_t.h>
+#include <SDL2/SDL_video.h>
 #include <stdbool.h>
 #include <stdint.h>
 
-int main(void) {
+static SDL_Window *window = NULL;
+static SDL_GLContext gl_ctx = NULL;
 
+static void init(void) {
   if (SDL_Init(SDL_INIT_VIDEO) < 0) {
     SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Unable to initialize SDL: %s",
                  SDL_GetError());
-    return 1;
+    exit(1);
   }
 
   SDL_LogSetAllPriority(SDL_LOG_PRIORITY_VERBOSE);
@@ -32,23 +34,24 @@ int main(void) {
   const char window_title[] = "hello";
   const uint64_t window_width = 1024;
   const uint64_t window_height = 768;
-  SDL_Window *window = SDL_CreateWindow(window_title, SDL_WINDOWPOS_CENTERED,
-                                        SDL_WINDOWPOS_CENTERED, window_width,
-                                        window_height, flags);
+  window = SDL_CreateWindow(window_title, SDL_WINDOWPOS_CENTERED,
+                            SDL_WINDOWPOS_CENTERED, window_width, window_height,
+                            flags);
 
   if (!window) {
     SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Unable to create window: %s",
                  SDL_GetError());
-    return 1;
+    exit(1);
   }
 
-  SDL_GLContext gl_ctx = SDL_GL_CreateContext(window);
+  gl_ctx = SDL_GL_CreateContext(window);
   if (!gl_ctx) {
     SDL_LogError(SDL_LOG_CATEGORY_ERROR,
                  "Unable to create context from window: %s", SDL_GetError());
-    return 1;
+    exit(1);
   }
 
   glEnable(GL_DEPTH_TEST);
   glEnable(GL_CULL_FACE);
 }
+int main(void) { init(); }
