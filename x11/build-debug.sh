4f99daf1e827a83fc52c11ee4d8c3a90257e01e6
Philippe Gaultier <philippe.gaultier@gi-de.com>
2023-05-11 17:27:46 +0200

    add warnings

--- x11/build-debug.sh
+++ x11/build-debug.sh
@@ -6,10 +6,13 @@ musl-gcc \
   -g3 \
   -static \
   -Wall \
+  -Wcast-align \
   -Wconversion \
   -Wdouble-promotion \
   -Wextra \
   -Wno-sign-conversion \
   -Wno-unused-function \
   -Wno-unused-parameter \
+  -Wnull-dereference \
+  -Wwrite-strings \
   -o x11_debug.bin
--- x11/build-release.sh
+++ x11/build-release.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
 
-musl-gcc -O2 main.c -Wl,--gc-sections -g -static -o x11_release.bin -Wall -Wextra
+musl-gcc \
+  -O2 \
+  main.c \
+  -static \
+  -Wl,--gc-sections \
+  -Wl,-s \
+  -Wall \
+  -Wcast-align \
+  -Wconversion \
+  -Wdouble-promotion \
+  -Wextra \
+  -Wno-sign-conversion \
+  -Wno-unused-function \
+  -Wno-unused-parameter \
+  -Wnull-dereference \
+  -Wwrite-strings \
+  -o x11_release.bin
