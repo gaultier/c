a520e0a9479534f49abd5dc6fcc0683f54526f18
Philippe Gaultier <philippe.gaultier@gi-de.com>
2023-05-11 17:12:50 +0200

    fix release script

--- x11/build-release.sh
+++ x11/build-release.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-cc -O2 main.c -Wl,--gc-sections -g -nostdlib -e start -static  -nodefaultlibs -nostartfiles -o x11_release.bin
+musl-gcc -O2 main.c -Wl,--gc-sections -g -static -o x11_release.bin -Wall -Wextra
