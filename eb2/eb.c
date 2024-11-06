3e78fec3ac08a1e72aa3acb84bcf6de95ba99364
Philippe Gaultier <philippe.gaultier@gi-de.com>
2024-11-06 16:45:22 +0100

    todo

--- eb2/eb.c
+++ eb2/eb.c
@@ -1,6 +1,7 @@
 #include "libc.c"
 
 // TODO: fork/exec/sleep/wait/poll(?)
+// TODO: build for all os/archs
 [[noreturn]] void _start() {
   char msg[] = "Hello world!";
   write(1, msg, sizeof(msg) - 1);
