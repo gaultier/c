06b239c1e3ffa538622eae33e05718e39e74e988
Philippe Gaultier <philippe.gaultier@gi-de.com>
2024-11-06 22:39:20 +0100

    freebsd impl for fork/wait

--- eb2/os_freebsd_x86_64.c
+++ eb2/os_freebsd_x86_64.c
@@ -9,3 +9,11 @@ void exit(int status) {
 ssize_t write(int fd, const void *buf, size_t count) {
   return syscall3(4, fd, (long)buf, (long)count);
 }
+
+int fork() { return (int)syscall0(2); }
+
+int wait4(int pid, int *status, int options, void *rusage) {
+  return (int)syscall4(7, pid, (long)status, (long)options, (long)rusage);
+}
+
+int wait(int *status) { return wait4(-1, status, 0, 0); }
