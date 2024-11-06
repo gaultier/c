624c71f468a767147ecbb943ac322a497d56a8fa
Philippe Gaultier <philippe.gaultier@gi-de.com>
2024-11-06 21:59:22 +0100

    rename

---
+++ eb2/main.c
@@ -0,0 +1,13 @@
+#include "libc.c"
+
+// TODO: fork/exec/sleep/wait/poll(?)
+// TODO: build for all os/archs
+int main(int argc, char *argv[]) {
+  (void)argc;
+  (void)argv;
+
+  char msg[] = "Hello world!";
+  write(1, msg, sizeof(msg) - 1);
+  fork();
+  return 0;
+}
