df3951424b32a41b644898fd9d3e6c2d7656b84f
Philippe Gaultier <philippe.gaultier@gi-de.com>
2024-11-06 16:19:38 +0100

    fix

--- eb2/build.sh
+++ eb2/build.sh
@@ -3,7 +3,7 @@ set -e
 
 CFLAGS="${CFLAGS}"
 WARNINGS="$(tr -s '\n' ' ' < compile_flags.txt)"
-TARGET_OS="${TARGET_OS:-$(uname)}"
+TARGET_OS="${TARGET_OS:-$(uname | tr '[:upper:]' '[:lower:]')}"
 TARGET_ARCH="${TARGET_ARCH:-$(uname -m)}"
 
 clang eb.c -nostdlib  -nostartfiles $WARNINGS -nobuiltininc -fuse-ld=lld -e _start -static $CFLAGS --target="$TARGET_ARCH"-"$TARGET_OS"
