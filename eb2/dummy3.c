7bff242efbc51c8e49b40bcd0ac6940c3beaaecd
Philippe Gaultier <philippe.gaultier@gi-de.com>
2024-11-08 11:22:07 +0100

    [wip]

--- eb2/dummy3.c
+++ eb2/dummy3.c
@@ -1,8 +1,26 @@
 #include <errno.h>
 #include <poll.h>
+#include <sys/procdesc.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include <sys/procdesc.h>
+
+// High level os-independent API:
+// - Create child process -> return (err, child_pid, child_fd) e.g. `fork() +
+// pidfd_open()` (Linux) or  `pdfork()` (FreeBSD)
+// - if child -> do your thing
+// - if parent:
+//   + wait with timeout on child_fd with `poll` or `select`
+//   + if timed out:
+//     * kill child with SIGKILL using child_fd e.g.
+//     `pidfd_send_signal(child_fd)` (Linux) or `close(child_fd)` (FreeBSD).
+//     * sleep a bit
+//     * retry
+//  + else: the child exited by itself
+//    * get exit status using `wait4(child_pid, &status, ...)`
+//    * if status == 0: exit(0)
+//    * else:
+//      - sleep a bit
+//      - retry
 
 int main(int argc, char *argv[]) {
   (void)argc;
@@ -32,16 +50,16 @@ int main(int argc, char *argv[]) {
         return errno;
       }
       if (err == 0) { // Timeout fired.
-	      // Kill the child.
-	      close(child_fd);
+        // Kill the child.
+        close(child_fd);
       } else { // Child finished by itself.
-	      // Get exit status of child.
-	      int status = 0;
-	      wait4(child_pid, &status, 0, 0); // Reap zombie.
+        // Get exit status of child.
+        int status = 0;
+        wait4(child_pid, &status, 0, 0); // Reap zombie.
 
-	      if (WIFEXITED(status) && 0 == WEXITSTATUS(status)) {
-		return 0;
-	      }
+        if (WIFEXITED(status) && 0 == WEXITSTATUS(status)) {
+          return 0;
+        }
       }
 
       sleep(1);
