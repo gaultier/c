f3aef49c0e8be053a8c8f119941796deacb73a16
Philippe Gaultier <philippe.gaultier@gi-de.com>
2024-11-09 13:40:03 +0100

    [wip]
    [wip]

--- eb2/linux_eb.c
+++ eb2/linux_eb.c
@@ -9,7 +9,7 @@
 int main(int argc, char *argv[]) {
   (void)argc;
 
-  uint32_t sleep_ms = 128 * 1000;
+  uint32_t sleep_ms = 128;
 
   for (int retry = 0; retry < 10; retry += 1) {
     int child_pid = fork();
@@ -58,7 +58,7 @@ int main(int argc, char *argv[]) {
     }
 
     sleep_ms *= 2;
-    usleep(sleep_ms);
+    usleep(sleep_ms * 1000);
 
     close(child_fd);
   }
---
+++ eb2/sigtimedwait.c
@@ -0,0 +1,63 @@
+#define _GNU_SOURCE
+#include <errno.h>
+#include <signal.h>
+#include <stdint.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+void on_sigchld(int sig) { (void)sig; }
+
+int main(int argc, char *argv[]) {
+  (void)argc;
+  signal(SIGCHLD, on_sigchld);
+
+  uint32_t sleep_ms = 128;
+
+  for (int retry = 0; retry < 10; retry += 1) {
+    int child_pid = fork();
+    if (-1 == child_pid) {
+      return errno;
+    }
+
+    if (0 == child_pid) { // Child
+      argv += 1;
+      if (-1 == execvp(argv[0], argv)) {
+        return errno;
+      }
+      __builtin_unreachable();
+    }
+
+    sigset_t sigset = {0};
+    sigemptyset(&sigset);
+    sigaddset(&sigset, SIGCHLD);
+
+    siginfo_t siginfo = {0};
+
+    struct timespec timeout = {
+        .tv_sec = sleep_ms / 1000,
+        .tv_nsec = (sleep_ms % 1000) * 1000 * 1000,
+    };
+
+    int sig = sigtimedwait(&sigset, &siginfo, &timeout);
+    if (-1 == sig && EAGAIN != errno) { // Error
+      return errno;
+    }
+    if (-1 != sig) { // Child finished.
+      if (WIFEXITED(siginfo.si_status) && 0 == WEXITSTATUS(siginfo.si_status)) {
+        return 0;
+      }
+    }
+
+    if (-1 == kill(child_pid, SIGKILL)) {
+      return errno;
+    }
+
+    if (-1 == wait(NULL)) {
+      return errno;
+    }
+
+    sleep_ms *= 2;
+    usleep(sleep_ms * 1000);
+  }
+  return 1;
+}
