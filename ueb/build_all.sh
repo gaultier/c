e22cb205c3a655466f249c9760a922cd40335480
Philippe Gaultier <philippe.gaultier@gi-de.com>
2024-11-10 23:33:08 +0100

    [wip]

--- ueb/build_all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-
-TARGET_ARCH=arm TARGET_OS=linux TARGET_ENV=eabihf ./build.sh
-TARGET_ARCH=aarch64 TARGET_OS=linux ./build.sh
-TARGET_ARCH=x86_64 TARGET_OS=linux ./build.sh
-TARGET_ARCH=x86_64 TARGET_OS=freebsd ./build.sh
