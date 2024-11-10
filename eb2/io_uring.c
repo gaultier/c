fe3a9ebb3e514d3fad3a489dd59177e8b76e3998
Philippe Gaultier <philippe.gaultier@gi-de.com>
2024-11-10 16:47:32 +0100

    [wip]

--- eb2/io_uring.c
+++ eb2/io_uring.c
@@ -1,8 +1,10 @@
 #define _GNU_SOURCE
+#include <assert.h>
 #include <errno.h>
 #include <liburing.h>
 #include <signal.h>
 #include <stdint.h>
+#include <sys/mman.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
@@ -14,11 +16,36 @@ int main(int argc, char *argv[]) {
 
   uint32_t wait_ms = 128;
 
-  struct io_uring_params params = {0};
-  int ring_fd = io_uring_setup(1, &params);
+  struct io_uring_params p = {0};
+  int ring_fd = io_uring_setup(1, &p);
   if (-1 == ring_fd) {
     return errno;
   }
+
+  assert(p.features & IORING_FEAT_SINGLE_MMAP);
+
+  int sring_sz = (int)(p.sq_off.array + p.sq_entries * (int)sizeof(unsigned));
+  int cring_sz =
+      (int)(p.cq_off.cqes + p.cq_entries * sizeof(struct io_uring_cqe));
+  if (cring_sz > sring_sz) {
+    sring_sz = cring_sz;
+  }
+  cring_sz = sring_sz;
+  void *sq_ptr = mmap(0, (size_t)sring_sz, PROT_READ | PROT_WRITE,
+                      MAP_SHARED | MAP_POPULATE, ring_fd, IORING_OFF_SQ_RING);
+  if (sq_ptr == MAP_FAILED) {
+    return errno;
+  }
+  void *cq_ptr = sq_ptr;
+  unsigned *sring_tail = (uintptr_t)sq_ptr + p.sq_off.tail;
+  unsigned *sring_mask = (uintptr_t)sq_ptr + p.sq_off.ring_mask;
+  unsigned *sring_array = (uintptr_t)sq_ptr + p.sq_off.array;
+
+  /* Map in the submission queue entries array */
+  struct io_urigin_sqe *sqes = mmap(
+      0, p.sq_entries * sizeof(struct io_uring_sqe), PROT_READ | PROT_WRITE,
+      MAP_SHARED | MAP_POPULATE, ring_fd, IORING_OFF_SQES);
+  if (sqes == MAP_FAILED) {
 
 #if 0
   for (int retry = 0; retry < 10; retry += 1) {
@@ -68,5 +95,5 @@ int main(int argc, char *argv[]) {
     wait_ms *= 2;
   }
 #endif
-  return 1;
-}
+    return 1;
+  }
