d5d3bd4311823b73fa0e26c76ef9800a2d1e8e63
Philippe Gaultier <philippe.gaultier@gi-de.com>
2024-11-11 10:02:54 +0100

    fix

--- ueb/io_uring.c
+++ ueb/io_uring.c
@@ -38,39 +38,31 @@ int main(int argc, char *argv[]) {
     io_uring_prep_waitid(sqe, P_PID, (id_t)child_pid, &si, WEXITED, 0);
     sqe->user_data = 1;
 
+    io_uring_submit(&ring);
+
     struct __kernel_timespec ts = {
         .tv_sec = wait_ms / 1000,
         .tv_nsec = (wait_ms % 1000) * 1000 * 1000,
     };
     struct io_uring_cqe *cqe = NULL;
 
-    int ret = io_uring_submit_and_wait_timeout(&ring, &cqe, 1, &ts, NULL);
+    int ret = io_uring_wait_cqe_timeout(&ring, &cqe, &ts);
 
     printf("ret=%d res=%d user_data=%llu status=%d exited=%d exit_status=%d\n",
            ret, cqe ? cqe->res : 0, cqe ? cqe->user_data : 0, si.si_status,
            WIFEXITED(si.si_status), WEXITSTATUS(si.si_status));
     // If child exited successfully: the end.
-    if (ret == 1 && NULL != cqe && cqe->res >= 0 && cqe->user_data == 1 &&
+    if (ret == 0 && cqe->res >= 0 && cqe->user_data == 1 &&
         WIFEXITED(si.si_status) && 0 == WEXITSTATUS(si.si_status)) {
       return 0;
     }
-
-    uint64_t user_data = cqe ? cqe->user_data : 0;
-    io_uring_cqe_seen(&ring, cqe);
-
-    kill(child_pid, SIGKILL);
-
-    // Drain the CQE.
-    if (1 != user_data) {
-      for (;;) {
-        ret = io_uring_wait_cqe(&ring, &cqe);
-        user_data = cqe ? cqe->user_data : 0;
-        io_uring_cqe_seen(&ring, cqe);
-        printf("[D002] %d\n", ret);
-        if (ret == 0 && 1 == user_data) {
-          break;
-        }
-      }
+    if (ret == 0) {
+      io_uring_cqe_seen(&ring, cqe);
+    } else {
+      kill(child_pid, SIGKILL);
+      // Drain the CQE.
+      ret = io_uring_wait_cqe(&ring, &cqe);
+      io_uring_cqe_seen(&ring, cqe);
     }
 
     wait(NULL);
@@ -78,4 +70,5 @@ int main(int argc, char *argv[]) {
     wait_ms *= 2;
     usleep(wait_ms * 1000);
   }
+  return 1;
 }
